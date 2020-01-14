d61e9cbdb687eeeb71469556e8c8b8e83d052edb
lvaquero <loreto@notikumi.com>
2020-01-14 21:38:44 +0100

    Add logic

---
+++ data/data.js
@@ -0,0 +1,88 @@
+const localMarkets = [
+    {
+        product: "Vieiras",
+        markets: [ 
+            {
+                city: "Madrid",
+                price: 500
+            },
+            {
+                city: "Barcelona",
+                price: 450
+            },
+            {
+                city: "Lisboa",
+                price: 600
+            }
+        ]
+    }, {   product: "Pulpo",
+        markets: [ 
+            {
+                city: "Madrid",
+                price: 0
+            },
+            {
+                city: "Barcelona",
+                price: 120
+            },
+            {
+                city: "Lisboa",
+                price: 100
+            }
+        ]
+
+    }, { 
+        product: "Centollo", 
+        markets: [
+            {
+                city: "Madrid",
+                price: 450
+            },
+            {
+                city: "Barcelona",
+                price: 0
+            },
+            {
+                city: "Lisboa",
+                price: 500
+            }
+        ],
+
+    } 
+
+];
+   
+const amount  = {
+    centollo : 50,
+    pulpo : 100,
+    vieira : 50
+}
+
+const distanceToCities= [
+    {
+        city: "Madrid",
+        distance: 800
+    },{
+        city: "Barcelona",
+        distance: 1100
+    },{
+        city: "Lisboa",
+        distance: 600
+    }
+];
+
+const costFixLoadTruck = 5;
+const costPerKm = 2;
+
+const lostPercent = 0.1;
+const lostKms = 1000;
+
+export {
+    localMarkets,
+    amount,
+    distanceToCities,
+    costFixLoadTruck,
+    costPerKm,
+    lostPercent,
+    lostKms
+  }
---
+++ js/main.js
@@ -0,0 +1,89 @@
+import { localMarkets, amount, distanceToCities, costFixLoadTruck, costPerKm, lostPercent, lostKms}  from "../data/data.js";
+
+// Calculo del beneficio producto/ciudad
+const profitProduct = (amount, product) => amount * product
+
+// A cada mercado local se le añade la propiedad del benecio "bruto" de cada producto
+const localMarketsWithprofit = localMarkets.filter( localMarket => {
+    if (localMarket.product === "Vieiras") {
+            localMarket.markets = localMarket.markets.map( market => {
+            return {
+                profit: profitProduct(amount.vieira, market.price),
+                ...market
+            };
+        });
+    } else if (localMarket.product === "Pulpo") {
+        localMarket.markets = localMarket.markets.map( market => {
+            return {
+                profit: profitProduct(amount.pulpo, market.price),
+                ...market
+            };
+        });
+    } else if (localMarket.product === "Centollo") {
+        localMarket.markets = localMarket.markets.map( market => {
+            return {
+                profit: profitProduct(amount.centollo, market.price),
+                ...market
+            };
+        });
+    }
+    return localMarkets;
+});
+
+// Calculo de pérdida valor de compra por kilómetro.
+const lostShell = (lostPercent, lostKms, distance) => {
+    let lostShell = lostPercent * lostKms * distance;
+    return lostShell;
+}
+
+// Calculo de pérdida valor por transporte
+const calculateTransportToCity = (fixedCost, costPerKm, distance) => {
+    let costTransport = fixedCost + costPerKm * distance;
+    return costTransport;
+}
+
+// Cálculo del beneficio bruto por ciudad
+const calculateprofitPerCity = (localMarketsWithprofit,city) => {
+    let cityMarket = localMarketsWithprofit.map(localMarket => localMarket.markets.filter(market => market.city === city));
+    let mergedCityMadrid = [].concat.apply([], cityMarket);
+    let profitCity = mergedCityMadrid.reduce((sum, value) => ( sum + value.profit ), 0);
+    return profitCity;
+}
+
+// Cálculo del beneficio neto por ciudad
+const calculateNetProfit = (localMarketsWithprofit, distanceToCity,costFixLoadTruck, costPerKm, lostPercent, lostKms ) => {
+    let netProfit = calculateprofitPerCity(localMarketsWithprofit, distanceToCity.city) 
+                    - calculateTransportToCity(costFixLoadTruck, costPerKm, distanceToCity.distance) 
+                    - lostShell(lostPercent, lostKms, distanceToCity.distance);
+    return netProfit;
+}
+
+// Cálculo del beneficio neto por ciudad
+const profitPerCity = (localMarketsWithprofit, distanceToCities) => {
+    let profitByCity = distanceToCities.map( distanceToCity => {
+        return {
+          cityProfit:  calculateNetProfit(localMarketsWithprofit, distanceToCity,costFixLoadTruck, costPerKm, lostPercent, lostKms ),
+          city: distanceToCity.city
+        };
+      });
+    
+    return profitByCity;
+}
+console.log(profitPerCity(localMarketsWithprofit, distanceToCities));
+
+// Cálculo de la ciudad con mayor beneficio
+const cityWinner =  profitPerCity(localMarketsWithprofit, distanceToCities).reduce((accum, cityCandidate) => {
+    if(cityCandidate.cityProfit > accum.cityProfit)
+        return cityCandidate;
+        return accum;
+  });
+  console.log(cityWinner);
+
+
+// Mostrar en pantalla
+const showSolution = () => {
+    const el = document.querySelector('.title');
+    el.innerHTML = cityWinner.city;
+}
+
+showSolution();
